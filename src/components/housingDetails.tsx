609235e842f74ecae5a34241050e11b88d14382d
mahilday <mathildaimadojiemu@gmail.com>
2021-09-09 11:34:48 +0100

    added responsivity

--- src/components/housingDetails.tsx
+++ src/components/housingDetails.tsx
@@ -8,7 +8,7 @@ function HousingDetails({dateFormatter, data}: IPermit){
     return(
         <div className="centerMain">
             <div><p className="text-blue bold small">{data.permit_}</p></div>
-            <div><p className="boldest small text-gray">{data.permit_type}</p><p className="small">{data.review_type}</p></div>
+            <div><p className="boldest smallScreen text-gray">{data.permit_type}</p><p className="small">{data.review_type}</p></div>
             <div className="work_des"><span className="boldest data_headers text-blue">Work Description</span>{data.work_description}</div>
             <div className="flex">
             <div className="work_des new"><span className="boldest data_headers text-blue">Application start Date</span>{dateFormatter(data.application_start_date)}</div>
